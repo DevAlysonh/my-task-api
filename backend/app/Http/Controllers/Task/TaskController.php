dd2230e3714af89f98ffd9659c61f862a54a8e88
devalysonh <dev.alysonh@gmail.com>
2025-03-15 20:02:44 -0300

    Feat: Create A new Route to create tasks

--- backend/app/Http/Controllers/Task/TaskController.php
+++ backend/app/Http/Controllers/Task/TaskController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\Task;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Task\CreateTask;
+use App\Http\Resources\Task\TaskResource;
+use Architecture\Application\Task\Dto\TaskInputDto;
+use Architecture\Application\Task\TaskService;
+use Illuminate\Http\Response;
 
 class TaskController extends Controller
 {
-    public function store(Request $request)
+    public function __construct(protected TaskService $service)
+    { }
+
+    public function store(CreateTask $request)
     {
-        return response()->json([], 204);
+        $task = TaskInputDto::fromArray($request->validated());
+        $created = $this->service->create($task);
+
+        return response()->json(
+            new TaskResource($created->refresh()),
+            Response::HTTP_CREATED
+        );
     }
 }
