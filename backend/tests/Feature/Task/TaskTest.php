097ed0d00a4f179401394c04c65ef8111d2eeab8
devalysonh <dev.alysonh@gmail.com>
2025-03-15 20:03:18 -0300

    Test: Improve tasktest class to be more specifc

--- backend/tests/Feature/Task/TaskTest.php
+++ backend/tests/Feature/Task/TaskTest.php
@@ -2,22 +2,27 @@
 
 namespace Tests\Feature\Task;
 
+use Architecture\Application\Task\TaskStatus;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function testIfUsersCanCreateANewTask(): void
+    public function test_UsersShouldCanCreateANewTask(): void
     {
         $arr = [
             'title' => 'Foo Bar',
-            'status' => 'pending',
         ];
 
         $response = $this->post('/api/tasks', $arr);
 
-        $response->assertStatus(204);
+        $response->assertStatus(Response::HTTP_CREATED);
+        $this->assertNotEmpty($response->getData());
+        $this->assertEquals($response->getData()->data->title, $arr['title']);
+        $this->assertEquals($response->getData()->data->status, TaskStatus::PENDING->value);
+        $this->assertEquals($response->getData()->message, 'New Task created Successfully');
     }
 }
