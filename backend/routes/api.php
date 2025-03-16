581969d23b2399a5ddb130accc7a1e094482c967
devalysonh <dev.alysonh@gmail.com>
2025-03-16 14:51:15 -0300

    Setup Authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\Task\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(TaskController::class)->group(function () {
+Route::middleware('auth:sanctum')->controller(TaskController::class)->group(function () {
     Route::post('tasks', 'store');
     Route::patch('tasks/{task}', 'update');
     Route::get('tasks/{task}', 'show');
     Route::delete('tasks/{task}', 'destroy');
 });
 
-Route::controller(CommentController::class)->group(function () {
+Route::middleware('auth:sanctum')->controller(CommentController::class)->group(function () {
     Route::post('comments', 'store');
     Route::delete('comments/{comment}', 'destroy');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::middleware('auth:sanctum')->delete('logout', 'destroy');
 });
