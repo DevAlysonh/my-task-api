f1add1c88679c2b161b609559539e090fb7b491a
devalysonh <dev.alysonh@gmail.com>
2025-03-16 00:53:53 -0300

    fix: fix task tests temporally

--- backend/achitecture/Application/Task/Dto/TaskInputDto.php
+++ backend/achitecture/Application/Task/Dto/TaskInputDto.php
@@ -6,20 +6,23 @@ use Exception;
 
 class TaskInputDto implements DtoInterface
 {
-    public function __construct(protected string $title)
-    { }
+    public function __construct(protected string $title, protected int $userId)
+    {
+    }
 
     public static function fromArray(array $data): self
     {
         if(empty($data['title'])) {
             throw new Exception('You should give a title for a new Task!');
         }
-
-        return new self($data['title']);
+        return new self($data['title'], $data['user_id']);
     }
 
     public function toArray(): array
     {
-        return ['title' => $this->title];
+        return [
+            'title' => $this->title,
+            'user_id' => $this->userId 
+        ];
     }
 }
